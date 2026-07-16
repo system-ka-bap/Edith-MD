1fb0f1a9b15ffc14de7278f230734079cb05ef13
system-ka-bap <azizmareeuk@gmail.com>
2026-07-16 16:18:44 +0500

    Update settings.js manually

--- settings.js
+++ settings.js
@@ -1,3 +1,6 @@
-export function helper() {
-  return true;
-}
+// settings.js
+
+module.exports = {
+  DATABASE: process.env.DATABASE_URL || "mongodb+srv://vogolet366_db_user:ZvGhMxM1Zy32JxOy@cluster21.uuli0qp.mongodb.net/?appName=Cluster21",
+    DB_NAME: "bandaheali"
+};
